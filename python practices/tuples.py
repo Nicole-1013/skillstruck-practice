af61a314f6c96c3b3d4100ba0635fe6db2e384fd
Nicole-1013 <ngalvan1013@gmail.com>
2024-08-30 14:04:48 -0700

    exported from Skill Struck

---
+++ python practices/tuples.py
@@ -0,0 +1,17 @@
+#grades = (95, 70, 85, 92, 100)
+#print(grades)
+#print(grades[-2])
+#print(grades[0:3])
+#------------------------------------------
+#friends = (3, 5, 7, 8, 10, 2, 12, 4)
+#user = int(input("Give me a number"))
+
+#if user in friends:
+   # print("Yes")
+#else:
+    #print("No")
+#------------------------------------------
+words = ("one", "two", "three", "four", "five", "six", "seven", "eight")
+print(words[5])
+print(words[-3])
+print(words[0:4])
