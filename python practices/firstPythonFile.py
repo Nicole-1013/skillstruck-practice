8c95d7f5a87e5559eccab3b51b8c5bb5215b2a0a
Nicole-1013 <ngalvan1013@gmail.com>
2024-08-30 14:04:45 -0700

    exported from Skill Struck

---
+++ python practices/firstPythonFile.py
@@ -0,0 +1,24 @@
+#friends = { "Shane" : 10, "Samantha" : 9, "Shiloh" : 12, "Sean" : 11 }
+
+#friends['Sebastian'] = 8
+#friends.pop("Shiloh")
+#print(friends)
+
+
+#shapes = {
+#"Triangle": 8,
+#"Circle": 15,
+#"Square": 10,
+#"Rectangle" : 12
+
+#}
+#user = input("Give me a new shape")
+#userHeight = int(input("Give me the height of the new shape"))
+
+#shapes[user] = userHeight
+#print(shapes)
+
+trees = { "aspen" : 75, "pine" : 82, "maple" : 60, "oak" : 65, "willow" : 80, "cottonwood" : 100 }
+user = input("What tree do you want to remove?")
+trees.pop(user)
+print(trees)
