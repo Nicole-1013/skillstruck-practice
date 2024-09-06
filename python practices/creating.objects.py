e059d008d756019c3795586d0612d9786e79eccc
Nicole-1013 <ngalvan1013@gmail.com>
2024-09-06 13:41:48 -0700

    exported from Skill Struck

---
+++ python practices/creating.objects.py
@@ -0,0 +1,34 @@
+#class Hat:
+    #def __init__(self, kind, color, material):
+        #self.kind = kind
+        #self.color = color
+        #self.material = material
+#myObject = Hat("chef", "red", "cotton")
+#print(myObject.color)
+#---------------------------------------
+#class Fruit:
+    #def __init__(self, shape, kind, size):
+        #self.shape = shape
+        #self.kind = kind
+        #self.size = size
+
+#fruit1 = Fruit("oval", "pear", "small")
+#fruit2 = Fruit("circle", "strawberry", "small")
+#fruit3 = Fruit("square", "apple", "small")
+#fruit4 = Fruit("oval", "melon", "medium")
+#print(fruit1.kind)
+#print(fruit2.kind)
+#print(fruit3.kind)
+#print(fruit4.kind)
+#-----------------------------------------------
+class Pet:
+    def __init__(self, breed, color, age):
+        self.breed = breed
+        self.color = color
+        self.age = age
+
+pet1 = Pet("corgi", "yellow", "4")
+pet2 = Pet("German Shepherd", "brown", "2")
+pet3 = Pet("Chihuahua", "black", "10")
+
+
